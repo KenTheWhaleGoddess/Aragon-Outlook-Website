a81cae8c1d9e696722a4a50262c6588d9e861ae4
Ken Preiser <kenpreiser@gmail.com>
2015-01-20 15:09:16 -0800

    MADE THE STRUCTURE LESS UGLY
    yeah

--- loop.php
+++ loop.php
@@ -4,20 +4,21 @@ Template Name: Loop
 */
 ?>
 
-<?php if ( have_posts() ) : ?>
+<?php if ( have_posts() )  ?>
 
 	<div id="loop" class="<?php if ($_COOKIE['mode'] == 'grid') echo 'grid'; else echo 'list'; ?> clear">
 
-	<?php while ( have_posts() ) : the_post(); ?>
+	<?php while ( have_posts() ) {
+	the_post(); ?>
 
 		<div <?php post_class('post clear'); ?> id="post_<?php the_ID(); ?>">
 
-			<?php if ( has_post_thumbnail() ) :?>
+			<?php if ( has_post_thumbnail() )  { ?>
 			<a href="<?php the_permalink() ?>" class="thumb" data-instant><?php the_post_thumbnail('thumbnail', array(
 						'alt'	=> trim(strip_tags( $post->post_title )),
 						'title'	=> trim(strip_tags( $post->post_title )),
-					)); ?></a>
-			<?php endif; ?>
+					)); } ?></a>
+			<?php } ?>
 
 			<div class="post-category"><?php the_category(' / '); ?></div>
 			<h2><a data-instant href="<?php the_permalink() ?>"><?php the_title(); ?></a></h2>
@@ -35,8 +36,8 @@ else
 			<div class="post-content"><?php if (function_exists('smart_excerpt')) smart_excerpt(get_the_excerpt(), 27); ?></div>
 		</div>
 
-	<?php endwhile; ?>
+	<?php } ?>
 
 	</div>
 
-<?php endif; ?>
+<?php } ?>
